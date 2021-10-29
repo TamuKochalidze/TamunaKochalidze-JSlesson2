0f9f72713c8729ae2b9f141120dbc481c7d4e296
Tamuna <85788033+tamuna-kochalidze@users.noreply.github.com>
2021-10-29 10:53:11 +0400

    number 2

--- script.js
+++ script.js
@@ -2,18 +2,34 @@
 //number1 შექმენით ფუნქცია, რომელსაც გადავცემთ n რაოდენობის მნიშვნელობას და დააბრუნოს მხოლოდ დადებითი რიცხვების ჯამს:
 // 2, -9, 5, 11, -30, 100, -8, -11, -4, 5, -6, 8;
 
-function sum(...args) {
-    let newSum = 0;
+// function sum(...args) {
+//     let newSum = 0;
     
-    for (let item of args) {
-        if (item > 0) {
-            newSum = newSum + item;
-        }
-    }
-    return newSum;
-}
+//     for (let item of args) {
+//         if (item > 0) {
+//             newSum = newSum + item;
+//         }
+//     }
+//     return newSum;
+// }
 
-let result = sum(2, -9, 5, 11, -30, 100, -8, -11, -4, 5, -6, 8);
-console.log(result);
+// let result = sum(2, -9, 5, 11, -30, 100, -8, -11, -4, 5, -6, 8);
+// console.log(result);
 
 //--------------------------------------------
+
+//number 2 
+//ფუნქციის საშულებით გამოიტანეთ ყველა რიცხვის ჯამი:
+// 10, 50, 6, 7, 8, 11, 6, 3, 9
+
+function sum(...args) {
+        let numberSum = 0; 
+    
+        for (let item of args) {
+            numberSum = numberSum + item
+        }
+        console.log(numberSum);
+    }
+sum(10, 50, 6, 7, 8, 11, 6, 3, 9)
+
+//-----------------------------------------
